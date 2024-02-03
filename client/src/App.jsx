726b18f8e37b357ad587e5b0e1e8049ea596583f
kotreshdm <kotreshdm@gmail.com>
2024-02-03 08:34:43 +0530

    Routes and pages added
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { About } from "./About";
+import SignIn from "./SignIn";
+import Search from "./Search";
+import SignUp from "./SignUp";
 
 function App() {
-  return <>Test</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/logout' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
