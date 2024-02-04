042fc449cbf2e4f419af12f6cb224b492c891698
kotreshdm <kotreshdm@gmail.com>
2024-02-04 17:22:16 +0530

    reduc toolkit added
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,20 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({
     email: "kotreshdm@gmail.com123",
     password: "Dell@2323",
   });
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -16,11 +22,10 @@ function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields.");
+      return dispatch(signInFailure("Please fill out all fields."));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart);
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -29,15 +34,15 @@ function SignIn() {
 
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -91,9 +96,9 @@ function SignIn() {
               Sign Up
             </Link>
           </div>
-          {errorMessage && (
+          {error && (
             <Alert className='mt-5' color='failure'>
-              {errorMessage}
+              {error}
             </Alert>
           )}
         </div>
