3515813688822144fdc06975d9f4f8404392a20f
kotreshdm <kotreshdm@gmail.com>
2024-02-09 11:14:10 +0530

    added api
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

---
+++ api/routes/user.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  signout,
+} from "../controllers/user.controler.js";
+import { verifyToken } from "../utils/verifyUser.js";
+const router = express.Router();
+
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
+
+export default router;
