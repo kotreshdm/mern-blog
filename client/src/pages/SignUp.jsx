c72ca953985faea62d502ae2eefb4e7a398a91fa
kotreshdm <kotreshdm@gmail.com>
2024-02-09 10:51:38 +0530

    removed o auth
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Oauth from "../components/Oauth";
+// import Oauth from "../components/Oauth";
 
 function SignUp() {
   const [formData, setFormData] = useState({
@@ -97,7 +97,7 @@ function SignUp() {
                 "Sign Up"
               )}
             </Button>
-            <Oauth />
+            {/* <Oauth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
