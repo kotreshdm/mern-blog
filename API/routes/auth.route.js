4fe1f0a529287735c16423e3c37f43c846c5b200
kotreshdm <kotreshdm@gmail.com>
2024-02-09 11:13:41 +0530

    deleted
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- API/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
-const router = express.Router();
-router.post("/sign-up", signup);
-router.post("/signin", signin);
-router.post("/google", google);
-
-export default router;
