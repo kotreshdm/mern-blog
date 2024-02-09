af27a2f3ed18c8a4e1001f5cbf5450309d4a87ef
kotreshdm <kotreshdm@gmail.com>
2024-02-09 09:53:33 +0530

    installation commit
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- API/index.js
+++ API/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
   .then(() => console.log("Mango db Connected!"))
   .catch((err) => console.log(err));
 
+const __dirname = path.resolve();
 app.listen(PORT, () => {
   console.log(`Server start at port ${PORT}!!!!!`);
 });
@@ -26,6 +28,13 @@ app.listen(PORT, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
